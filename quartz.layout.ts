7586c3eee97d0c69ebdc88297c382b3377559df3
bartolo05 <vdbroeckb@gmail.com>
2024-11-16 20:10:33 +0100

    Quartz sync: Nov 16, 2024, 8:10 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,15 +1,43 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
 
+// MMW: hiding notes from graph via tags
+const tagsToRemove = ["graph-exclude"]
+const graphConfig = {
+  localGraph: {
+    removeTags: tagsToRemove,
+    excludeTags: ["graph-exclude"],
+  },
+  globalGraph: {
+    removeTags: tagsToRemove,
+    excludeTags: ["graph-exclude"],
+  },
+}
+
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [],
-  afterBody: [],
+  header: [Component.LinksHeader()],
+  afterBody: [
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        // from data-repo
+        repo: "morrowind-modding/morrowind-modding.github.io",
+        // from data-repo-id
+        repoId: "R_kgDOLP1-Jw",
+        // from data-category
+        category: "Announcements",
+        // from data-category-id
+        categoryId: "DIC_kwDOLP1-J84CdRF8",
+        reactionsEnabled: true,
+      },
+    }),
+  ],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/morrowind-modding/wiki",
+      "Discord Community": "https://discord.gg/UdM3VrmJsc",
     },
   }),
 }
@@ -21,16 +49,28 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    Component.MobileOnly(Component.TableOfContents2()),
   ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+        filterFn: (node) => node.name !== "templates",
+      }),
+    ),
   ],
   right: [
-    Component.Graph(),
+    Component.MobileOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+        filterFn: (node) => node.name !== "templates",
+      }),
+    ),
+    Component.DesktopOnly(Component.Graph(graphConfig)),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
@@ -38,13 +78,33 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    Component.ContentMeta(),
+    Component.MobileOnly(Component.TableOfContents2()),
+  ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
+    Component.DesktopOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+        filterFn: (node) => node.name !== "templates",
+      }),
+    ),
   ],
-  right: [],
+  right: [
+    Component.MobileOnly(
+      Component.Explorer({
+        folderClickBehavior: "link",
+        filterFn: (node) => node.name !== "templates",
+      }),
+    ),
+    Component.DesktopOnly(Component.Graph(graphConfig)),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
+  ],
 }
