36be286a0f59b80fcb4b6defa80afc116c6e1f20
bartolo05 <vdbroeckb@gmail.com>
2024-11-16 21:51:31 +0100

    Quartz sync: Nov 16, 2024, 9:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Vilenought Wiki",
+    pageTitle: "Apvaÿa Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
